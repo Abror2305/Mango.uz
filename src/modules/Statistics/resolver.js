969d15c185a8956a7a7784b7db1d409c42f73411
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-04-18 11:46:49 +0500

    add mostSoldProduct and lessSoldProduct

--- src/modules/Statistics/resolver.js
+++ src/modules/Statistics/resolver.js
@@ -43,6 +43,103 @@ export default {
                     message: error.message
                 }
             }
-        }
+        },
+        mostSoldProduct: (_,{ limit }, {read, token, userAgent }) => {
+            try {
+                const userId = checkToken(token, userAgent)
+
+                if (!userId) {
+                    return new ForbiddenError("Invalid token")
+                }
+
+                if (!isAdmin(userId, read)) {
+                    return new ForbiddenError("You are not authorized to perform this action")
+                }
+                const orders = read('orders')
+                const products = read('products')
+                const productIds = orders.reduce((acc, order) => {
+                    if(!order.isPaid){
+                        return acc
+                    }
+                    acc.push(order.productId)
+                    return acc
+                }, [])
+                const productIdsCount = productIds.reduce((acc, productId) => {
+                    if(acc[productId]){
+                        acc[productId]++
+                    } else {
+                        acc[productId] = 1
+                    }
+                    return acc
+                }, {})
+                const productIdsCountArr = Object.keys(productIdsCount).map(key => {
+                    return {
+                        product: products.find(product => +product.id === +key),
+                        count: productIdsCount[key]
+                    }
+                })
+                productIdsCountArr.sort((a, b) => {
+                    return b.count - a.count
+                })
+                if(limit){
+                    console.log(productIdsCountArr.slice(0, limit))
+                    return productIdsCountArr.slice(0, limit)
+                }
+                return productIdsCountArr
+            } catch (error) {
+                return {
+                    status: 400,
+                    message: error.message
+                }
+            }
+        },
+        leastSoldProduct: (_,{limit},{read,token,userAgent}) => {
+            try {
+
+                const userId = checkToken(token, userAgent)
+                if (!userId) {
+                    return new ForbiddenError("Invalid token")
+                }
+
+                if (!isAdmin(userId, read)) {
+                    return new ForbiddenError("You are not authorized to perform this action")
+                }
+                const orders = read('orders')
+                const products = read('products')
+                const productIds = orders.reduce((acc, order) => {
+                    if(!order.isPaid){
+                        return acc
+                    }
+                    acc.push(order.productId)
+                    return acc
+                }, [])
+                const productIdsCount = productIds.reduce((acc, productId) => {
+                    if(acc[productId]){
+                        acc[productId]++
+                    } else {
+                        acc[productId] = 1
+                    }
+                    return acc
+                }, {})
+                const productIdsCountArr = Object.keys(productIdsCount).map(key => {
+                    return {
+                        product: products.find(product => +product.id === +key),
+                        count: productIdsCount[key]
+                    }
+                })
+                productIdsCountArr.sort((a, b) => {
+                    return a.count - b.count
+                })
+                if(limit){
+                    return productIdsCountArr.slice(0, limit)
+                }
+                return productIdsCountArr
+            } catch (error) {
+                return {
+                    status: 400,
+                    message: error.message
+                }
+            }
+        },
     }
 }
